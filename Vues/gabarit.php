be94d204c5c9cf442e2c2c7fd66010b5cc05fea0
Allan Delesvaux <delesvaux.allan@gmail.com>
2023-01-12 14:23:47 +0100

    Initialisation du projet et implementation du framework front tailwind

---
+++ Vues/gabarit.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="./output.css" rel="stylesheet">
+        <title>My sweet MVC</title>
+
+    </head>
+    <body>
+        <?php Vue::montrer('standard/entete'); ?>
+        <?php echo $A_vue['body'] ?>
+        <?php Vue::montrer('standard/pied'); ?>
+    </body>
+</html>
