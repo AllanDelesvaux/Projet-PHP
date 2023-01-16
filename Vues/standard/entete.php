19a00355a5f99caaa5ce6de805d3f3846ac2c3bf
Allan Delesvaux <delesvaux.allan@gmail.com>
2023-01-16 09:44:56 +0100

    Ajout zone de recherche Vue

--- Vues/standard/entete.php
+++ Vues/standard/entete.php
@@ -11,10 +11,7 @@
                             <img src="./assets/bx-x.svg" alt="quitter"/>
                         </button>
                     </div>
-                    <div class="flex items-center justify-center bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <img src="./assets/bx-search.svg" alt="recherche" class="px-4 py-2 rounded-md"/>
-                        <input class="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Recherche"/> <!--zone de recherche -->
-                    </div>
+                    <?php Vue::montrer('standard/composant/zoneDeRecherche'); ?>
 
                 </div>
             </div>
@@ -59,10 +56,7 @@
 
 </nav>
 
-<script>
-    function toggleModal(){
-        document.getElementById('modal').classList.toggle("hidden");
-        document.getElementById('elementFlou').classList.toggle("hidden");
-    }
+<script src="modal.js">
+    
 </script>
 
