ef82d6e1e9a484b1dc697702f537c1655820acb0
RIYADLARBAOUI <riyad.larbaoui@etu.univ-amu.fr>
2023-01-16 09:32:41 +0100

    ajout de nouveaux composants

--- Vues/standard/composant/lien.php
+++ Vues/standard/composant/lien.php
@@ -1,1 +1,3 @@
-<?php
+<a href="#responsive-header" class=" block mt-4 lg:inline-block lg:mt-0  px-4 py-2  mr-2">
+    Recherche
+</a>
--- Vues/standard/composant/zoneDeRecherche.php
+++ Vues/standard/composant/zoneDeRecherche.php
@@ -1,1 +1,1 @@
-<?php
+<input class="border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Recherche"/> <!--zone de recherche -->
