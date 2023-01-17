680b1eb4fee22647c475504132ef870465663ae4
Pinel-Guinard Bertil <bertil.pinel-guinard@etu.univ-amu.fr>
2023-01-17 11:43:10 +0100

    mise en fonctionnement de l'autoload

---
+++ Controleurs/ControleurLogin.php
@@ -0,0 +1,59 @@
+<?php
+    require '../Noyau/ChargementAuto.php';
+
+
+    final class ControleurLogin
+    {
+        public function defautAction()
+        {
+            //$O_helloworld =  new Helloworld();
+            //Vue::montrer('helloworld/voir', array('helloworld' =>  $O_helloworld->donneMessage()));
+
+            $login =$_POST['email'];
+            $password =$_POST['password'];
+
+            $O_conn = new ConnectionDatabase();
+            $O_conn = $O_conn->getConnection();
+            $_requete = "SELECT identifiant,mot_de_passe from Utilisateur ";
+            $O_statement = $O_conn->prepare($_requete);
+            $O_statement->execute();
+            $O_statement->setFetchMode(PDO::FETCH_ASSOC); // FETCH_ASSOC
+            $result = $O_statement->fetch();
+
+            if(!(empty($result)))
+            {
+                echo 'Erreur de requete <br>';
+                echo 'Requete: ' . $_requete;
+                exit();
+            }
+            else{
+            for ($i = 0; $i < sizeof($result); ++$i)
+                {
+                    if($result[$i]['identifiant']===$login && $result[$i]['mdp']===$password)
+                    {
+                        $_SESSION['suid'] = session_id();
+                        
+                        //redirection vers page connecté
+                        exit();
+                    }    
+
+                }
+                $_SESSION['error'] = 1;
+                var_dump($_SESSION['error']);
+                //header(' Location: /Vues/standard/composant/formulaire.php');
+            }
+        
+        }
+    
+        public function testformAction(Array $A_parametres = null, Array $A_postParams = null)
+        {
+    
+            Vue::montrer('helloworld/testform', array('formData' =>  $A_postParams));
+    
+        }
+    
+    }
+
+    
+
+ ?>
