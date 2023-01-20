955ad2de3d975f665d9f453eb7bc8f8cdf8d0f8a
Allan Delesvaux <delesvaux.allan@gmail.com>
2023-01-20 12:09:50 +0100

    Rectif position recherche

--- Vues/VuePage/entete.php
+++ Vues/VuePage/entete.php
@@ -1,16 +1,16 @@
-<nav class="flex items-start justify-start bg-white py-4 lg:px-12 shadow border-solid border-t-2">
+<nav class="flex items-start justify-start bg-black py-4 lg:px-12 shadow border-solid border-t-2">
 
     <div class="relative z-10" id="principal">
         <div class="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" id="elementFlou"></div>
         <div class="hidden fixed inset-0 z-10 overflow-y-auto" id="modal">
-            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
-                <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg bg-500-yellow">
-                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                        <button type="button" class="inline-flex w-full justify-center   px-4 py-2   sm:ml-3 sm:w-auto sm:text-sm" onclick="toggleModal()">
+            <div class="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0 ">
+                <div class="relative transform overflow-hidden rounded-lg bg-black text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg bg-500-yellow">
+                    <div class="bg-white px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                        <button type="button" class="inline-flex w-full justify-center px-8 py-2 sm:ml-3 sm:w-auto sm:text-sm" onclick="toggleModal()">
                             <img src="./assets/bx-x.svg" alt="quitter"/>
                         </button>
+                        <?php Vue::montrer('Composant/zoneDeRecherche'); ?>
                     </div>
-                    <?php Vue::montrer('Composant/zoneDeRecherche'); ?>
                 </div>
             </div>
         </div>
