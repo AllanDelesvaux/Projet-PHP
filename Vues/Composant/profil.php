37b97d5f8888475b0e60c563bd692776d21623e9
Allan Delesvaux <delesvaux.allan@gmail.com>
2023-01-24 11:17:28 +0100

    Ajout icone de profil et vue de la page de profil

--- Vues/Composant/profil.php
+++ Vues/Composant/profil.php
@@ -1,1 +1,3 @@
-<a href="/" > <img class="mx-auto h-10 w-auto" src='/assets/profil.png' alt ='photo de profil'/> </a>
+<div class="flex -space-x-2 overflow-hidden">
+  <img href="/" class="inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+</div>
