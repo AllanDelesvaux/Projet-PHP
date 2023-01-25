03184c984505c0dd4b62c70b6ba25751fec9bd0d
RIYADLARBAOUI <riyad.larbaoui@etu.univ-amu.fr>
2023-01-25 22:41:05 +0100

    modification page principale

--- Vues/VuePage/principale.php
+++ Vues/VuePage/principale.php
@@ -1,7 +1,6 @@
 <section class="bg-white dark:bg-zinc-800">
     <div class="container px-6 py-10 mx-auto">
-        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
-            <?php Vue::montrer('Composant/etiquette'); ?>
+        <div class="grid grid-cols-1 gap-8 md:grid-cols-3 xl:grid-cols-3">
             <?php Vue::montrer('Composant/etiquette'); ?>
             <?php Vue::montrer('Composant/etiquette'); ?>
             <?php Vue::montrer('Composant/etiquette'); ?>
