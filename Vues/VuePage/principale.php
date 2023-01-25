8ae1dbb559ca89e05682141cef8fb86f5be7d439
RIYADLARBAOUI <riyad.larbaoui@etu.univ-amu.fr>
2023-01-25 23:03:22 +0100

    ajout catégories sur page principale

--- Vues/VuePage/principale.php
+++ Vues/VuePage/principale.php
@@ -6,4 +6,10 @@
             <?php Vue::montrer('Composant/etiquette'); ?>
         </div>
     </div>
+
+    <section class="container px-2 py-10 mx-auto ">
+        <h1 class="font-bold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white mb-5">Categories</h1>
+        <?php Vue::montrer('Composant/categories') ?>
+    </section>
+
 </section>
