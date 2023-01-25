b7f383b17db3c16391317413ab27a1beae1f567e
Allan Delesvaux <delesvaux.allan@gmail.com>
2023-01-25 20:40:27 +0100

    ajout page principale

--- Controleurs/ControleurDefaut.php
+++ Controleurs/ControleurDefaut.php
@@ -4,6 +4,8 @@ final class ControleurDefaut
 {
     public function defautAction()
     {
+        Vue::montrer('Composant/principaleHaut');
+        Vue::montrer('Composant/principale');
         Vue::montrer('VuePage/recette');
     }
 
