f49d23500d7872fa1fcd05f39772a406bf187e0d
Pinel-Guinard Bertil <bertil.pinel-guinard@etu.univ-amu.fr>
2023-01-26 19:57:44 +0100

    config tailwind js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: ["./**/*.{html,js,php}"],
   theme: {
-    extend: {},
     extend: {
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
@@ -45,5 +44,16 @@ module.exports = {
   ]
     }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
