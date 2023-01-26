0af54cae35e7126842fe8ca7a6665e7aee216f62
Pinel-Guinard Bertil <bertil.pinel-guinard@etu.univ-amu.fr>
2023-01-26 20:57:14 +0100

    reinitialisation de tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./**/*.{html,js,php}"],
   theme: {
+    extend: {},
     extend: {
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
@@ -44,16 +45,5 @@ module.exports = {
   ]
     }
   },
-  plugins: [require("daisyui")],
-
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
+  plugins: [],
 }
